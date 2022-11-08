d5e051d47fe921615f94ce6e79fc063e36bd23ff
aymen2782 <aymen.sellaouti@gmail.com>
2022-11-08 17:34:07 +0100

    =update et delete

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -37,4 +37,35 @@ class PersonneController extends AbstractController
             'personne' => $personne,
         ]);
     }
+
+    #[Route('/personne/{id}/{name}/{age}', name: 'update_personne')]
+    public function updatePersonne($name, $age, $id): Response
+    {
+        $manager = $this->doctrine->getManager();
+        $repository = $this->doctrine->getRepository(Personne::class);
+        $personne = $repository->find($id);
+        $personne->setName($name);
+        $personne->setAge($age);
+        /* Ajouter dans la transaction  */
+        $manager->persist($personne);
+
+        /* Exécuter la transaction */
+        $manager->flush();
+        return $this->render('personne/detail.html.twig', [
+            'personne' => $personne,
+        ]);
+    } 
+    
+    #[Route('/personne/{id}', name: 'delete_personne')]
+    public function deletePersonne($id): Response
+    {
+        $manager = $this->doctrine->getManager();
+        $repository = $this->doctrine->getRepository(Personne::class);
+        $personne = $repository->find($id);
+        /* Ajouter dans la transaction  */
+        $manager->remove($personne);
+        /* Exécuter la transaction */
+        $manager->flush();
+        return $this->redirectToRoute('app_personne');
+    }
 }
